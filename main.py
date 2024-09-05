eb038068adb2146a7d906e82fb806c2e64d07c72
Morteza Khosravi <mortezakhosravi74@gmail.com>
2024-09-05 19:41:19 +0330

    ollama3.1, parser, captcha code handeler added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, extract_body_content, split_dom_content, clean_body_content
+from parse import parse_with_ollama
 
 st.title('AI Web Scraper')
 url = st.text_input("Enter a Website URL: ")
@@ -7,6 +8,20 @@ url = st.text_input("Enter a Website URL: ")
 if st.button("Scrape Site"):
     st.write("Scraping the website ... ")
     result = scrape_website(url)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
 
-    print(result)
+    st.session_state.dom_content = cleaned_content
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height= 300)
     
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe What you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the contnet")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)    
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
